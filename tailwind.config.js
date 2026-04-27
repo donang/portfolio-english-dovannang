8365fbb68d1e4dd5d0d14e82f2e9023505e4a285
Do Van Năng <donang198@gmail.com>
2026-04-27 21:49:24 +0700

    fix(fonts): remove unsupported Syne font to fix vietnamese characters, force SVN-Janelotus for scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ export default {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
         heading: ['Inter', 'sans-serif'],
-        script: ['"SVN-Janelotus"', '"Platinum Signature"', 'cursive'],
+        script: ['"SVN-Janelotus"', 'cursive'],
       },
       boxShadow: {
         '3d': '0 10px 30px -10px rgba(0,0,0,0.5), inset 0 1px 1px rgba(255,255,255,0.1)',
